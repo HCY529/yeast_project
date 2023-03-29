ac07fd3f2821e06a299eaa6954213e68b548db94
HCY529 <118234620+HCY529@users.noreply.github.com>
2023-03-29 10:53:41 +0800

    Update gene_data_download_sra.py

--- scripts/gene_data_download_sra.py
+++ scripts/gene_data_download_sra.py
@@ -12,6 +12,9 @@ with open(number_sample_list) as gene_information:
         number = number_sample[0]
         sample = number_sample[1]
 
-        cmd_str = "wget wget https://sra-pub-run-odp.s3.amazonaws.com/sra/{number}/{number}\nfastq-dump {number}\nfastqc {number}\nhisat2 --dta -t -p 8 -x {reference_file}yeast_reference -U {fastq_file}{number}.fastq |samtools view -bS -|samtools sort - -o {sample}.srt.bam\nsamtools index {sample}.srt.bam".format(number = number,reference_file = reference_file,fastq_file = fastq_file, sample=sample)
+        cmd_str = "wget wget https://sra-pub-run-odp.s3.amazonaws.com/sra/{number}/{number}
+        \nfastq-dump {number}\nfastqc {number}
+        \nhisat2 --dta -t -p 8 -x {reference_file}yeast_reference -U {fastq_file}{number}.fastq |samtools view -bS -|samtools sort - -o {sample}.srt.bam
+        \nsamtools index {sample}.srt.bam".format(number = number,reference_file = reference_file,fastq_file = fastq_file, sample=sample)
         print(cmd_str)
 
